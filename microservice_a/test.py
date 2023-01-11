f2f0ec19076f7ce53eb111b1792ee2a21b592020
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 19:42:10 +0100

    fix hang of test

--- microservice_a/test.py
+++ microservice_a/test.py
@@ -5,20 +5,23 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', type=int, required=True, help='Port number to test')
+parser.add_argument('--status-code', type=int, required=False, help='Expected HTTP status code', default=200)
 args = parser.parse_args()
 
 url_to_test = 'http://localhost' + ':' + str(args.port)
-expected_code = 200
+expected_code = args.status_code
 
 def test_microservice(url: str, expected_status_code: int):
     retries = 0
-    max_retries = 3
+    max_retries = 30
     while True:
         try:
             response = requests.get(url)
             if response.status_code == expected_status_code:
                 print(f'Microservice returned {response.status_code}')
                 break
+            else:
+                raise Exception(f'Unexpected status code. Got {response.status_code}')
         except requests.exceptions.RequestException as e:
             retries += 1
             if retries > max_retries:
