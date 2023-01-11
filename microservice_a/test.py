06f3b2e27214bacf589bb6f6744cda8ebc473fad
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 13:34:39 +0100

    fix

--- microservice_a/test.py
+++ microservice_a/test.py
@@ -1,10 +1,17 @@
 import os
 import requests
+import time
 
-def test_microservice(url: str, expected_status_code: int):
-    response = requests.get(url)
+def test_microservice(url: str, expected_status_code: int, retries: int = 10, wait: int = 5):
+    for i in range(retries):
+        response = requests.get(url)
+        if response.status_code == expected_status_code:
+            print(f'Microservice returned {response.status_code}')
+            return
+        else:
+            print(f'Microservice returned {response.status_code}. Retrying in {wait} seconds...')
+            time.sleep(wait)
     assert response.status_code == expected_status_code, f'Microservice returned {response.status_code}'
-    print(f'Microservice returned {response.status_code}')
 
 if __name__ == '__main__':
     test_microservice("http://localhost:5000", 200)
