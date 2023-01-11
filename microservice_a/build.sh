8928d5bc2191c15837064bbd83650accfad426ec
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 15:35:25 +0100

    fix

--- microservice_a/build.sh
+++ microservice_a/build.sh
@@ -4,17 +4,16 @@ set -xe
 
 __SVC=$( echo "$PWD" | awk -F '/' '{print $NF}')
 
-# just checking
-[[ -z ${PWD} ]] && exit 1
-[[ -z $1 ]] && exit 1
+# just checking null
+[[ -z ${PWD} ]] \
+  || [[ -z $1 ]] \
+  || [[ -z $IMAGE_PREFIX ]] \
+  && exit 1
 
-echo "building $__SVC"
-echo "tag is $1"
+echo "building $__SVC with tag $1"
 
 docker build \
   --file Dockerfile \
-  -t "ghcr.io/qb14-0985-4a94-939e-12e4f9cf1e8d/test-a-d/${__SVC}:${1}" \
-  -t "ghcr.io/qb14-0985-4a94-939e-12e4f9cf1e8d/test-a-d/${__SVC}:latest" \
-  -t "${__SVC}:${1}" \
-  -t "${__SVC}:latest" \
+  -t "${IMAGE_PREFIX}/${__SVC}:${1}" \
+  -t "${IMAGE_PREFIX}/${__SVC}:latest" \
   .
