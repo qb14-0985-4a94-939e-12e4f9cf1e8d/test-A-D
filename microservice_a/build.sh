1d59d9b1ecbf71b12f115cc66029a240ff5b6b18
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 12:29:05 +0100

    fix

--- microservice_a/build.sh
+++ microservice_a/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# for sure it would be better
+# https://docs.docker.com/build/cache/backends/gha/
+# but okay
+
 __SVC=$( echo "$PWD" | awk -F '/' '{print $NF}')
 
 # just checking
@@ -8,3 +12,11 @@ __SVC=$( echo "$PWD" | awk -F '/' '{print $NF}')
 
 echo "building $__SVC"
 echo "tag is $1"
+
+docker build \
+  --file Dockerfile \
+  -t "${__SVC}:${1}" \
+  -t "${__SVC}:latest" \
+  .
+
+docker images
