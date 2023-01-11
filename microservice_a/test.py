a71513e0a7cad077f595ec5dac661916d2aa0d4c
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 15:49:31 +0100

    fix

--- microservice_a/test.py
+++ microservice_a/test.py
@@ -1,8 +1,12 @@
 import os
 import requests
 import time
+import sys
 
 def test_microservice(url: str, expected_status_code: int, port: int = 5000, retries: int = 3, wait: int = 5):
+    if len(sys.argv) < 2:
+        print("Please provide port as an argument")
+        sys.exit()
     for i in range(retries):
         response = requests.get(f'{url}:{port}')
         if response.status_code == expected_status_code:
