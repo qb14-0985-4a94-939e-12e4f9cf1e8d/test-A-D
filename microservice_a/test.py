232c4317a352811d48f316abb1048c2052d74596
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 16:03:41 +0100

    fix

--- microservice_a/test.py
+++ microservice_a/test.py
@@ -1,21 +1,30 @@
 import os
+import argparse
 import requests
 import time
-import sys
 
-def test_microservice(url: str, expected_status_code: int, port: int = 5000, retries: int = 3, wait: int = 5):
-    if len(sys.argv) < 2:
-        print("Please provide port as an argument")
-        sys.exit()
-    for i in range(retries):
-        response = requests.get(f'{url}:{port}')
-        if response.status_code == expected_status_code:
-            print(f'Microservice returned {response.status_code}')
-            return
-        else:
-            print(f'Microservice returned {response.status_code}. Retrying in {wait} seconds...')
-            time.sleep(wait)
-    assert response.status_code == expected_status_code, f'Microservice returned {response.status_code}'
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type=int, required=True, help='Port number to test')
+args = parser.parse_args()
+
+url_to_test = 'http://localhost' + ':' + str(args.port)
+expected_code = 200
+
+def test_microservice(url: str, expected_status_code: int):
+    retries = 0
+    max_retries = 30
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == expected_status_code:
+                print(f'Microservice returned {response.status_code}')
+                break
+        except requests.exceptions.RequestException as e:
+            retries += 1
+            if retries > max_retries:
+                raise e
+            print(f'Microservice failed with error: {e}. Retrying in 5 seconds.')
+            time.sleep(5)
 
 if __name__ == '__main__':
-    test_microservice("http://localhost", 200,port= int(sys.argv[1]))
+    test_microservice(url_to_test, expected_code)
