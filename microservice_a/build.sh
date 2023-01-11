abb2c9a76373dfd21e8a5a4e4f4890931cf9a9ca
SCSCSC <122440112+qb14-0985-4a94-939e-12e4f9cf1e8d@users.noreply.github.com>
2023-01-11 18:18:07 +0100

    fix

--- microservice_a/build.sh
+++ microservice_a/build.sh
@@ -12,6 +12,13 @@ __SVC=$( echo "$PWD" | awk -F '/' '{print $NF}')
 
 echo "building $__SVC with tag $1"
 
+# my docker and docker on github actions runner does not work with --link option
+# but I still want to use symlinks
+# so, I will just leave those files symlinks and add making hardlinks from symlinks
+# when running pipeline
+
+find ./ -type l -exec sh -c 'ln -fn $(readlink "{}") "{}"' \;
+
 docker build \
   --file Dockerfile \
   -t "${IMAGE_PREFIX}/${__SVC}:${1}" \
